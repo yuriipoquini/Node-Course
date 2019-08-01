7293407e54a42e4bf5b4f5797a7079d23f8a1882
Yuri Poquini <gabril.yuri.1906@gmail.com>
2019-08-01 16:49:49 -0300

    Aula Introdutoria - Modules, NPM

---
+++ Section3.0/exports-app/app.js
@@ -0,0 +1,6 @@
+const utils = require('./utils');
+
+const result = utils(1, 3);
+
+
+console.log(result);
---
+++ Section3.0/notes-app/app.js
@@ -0,0 +1,3 @@
+const fs = require('fs');
+
+fs.appendFileSync('notes.txt', '\nI want to eat McDonalds');
