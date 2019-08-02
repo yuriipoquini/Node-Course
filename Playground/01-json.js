da7e08a048049dd55b6f7680fb5199b42e0a7710
Yuri Poquini <gabril.yuri.1906@gmail.com>
2019-08-01 22:26:15 -0300

    Challenge - Write and Read JSON

--- Playground/01-json.js
+++ Playground/01-json.js
@@ -1,10 +1,21 @@
 const fs = require('fs');
 
-const book = {
-    title: 'Assim falou Zaratrusta',
-    author: 'Nietzsche'
-};
+//const book = {
+//    title: 'Assim falou Zaratrusta',
+//    author: 'Nietzsche'
+//};
 
-const bookJSON = JSON.stringify(book);
+//const bookJSON = JSON.stringify(book);
 
-fs.writeFileSync('01-json.json', bookJSON);
+//fs.writeFileSync('01-json.json', bookJSON);
+
+
+const dataBuffer = fs.readFileSync('01-json.json');
+const dataJSON = dataBuffer.toString();
+const data = JSON.parse(dataJSON);
+
+data.name = "Yuri";
+data.age = 21;
+
+const newData = JSON.stringify(data)
+fs.writeFileSync('01-json.json', newData);
