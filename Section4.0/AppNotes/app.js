f99c2e8bf61b03fe82cf2327289f146d91cdbbd8
Yuri Poquini <gabril.yuri.1906@gmail.com>
2019-08-23 16:50:34 -0300

    calling notes methods in respectly handlers

--- Section4.0/AppNotes/app.js
+++ Section4.0/AppNotes/app.js
@@ -1,16 +1,18 @@
 const yargs = require('yargs');
+const notes = require('./notes.js');
 
 //Customize yargs version
 
-yargs.version('1.1.0')
+yargs.version('1.1.0');
 
+console.log(notes.getNotes());
 //Creating new commands
 yargs.command({
     command: 'add',
     describe: 'Adiciona uma Nota',
     builder: {
         title: {
-            describe: 'title',
+            describe: 'Note title',
             demandOption: true,
             type: 'string'
         },
@@ -22,33 +24,47 @@ yargs.command({
         }
 
     },
-    handler: function(argv) {
-        console.log('Titulo: ' + argv.title + '\nBody: ' + argv.body);
+    handler(argv) {
+        notes.addNotes(argv.title, argv.body);
     }    
-})
+});
 
 yargs.command({
     command: 'remove',
-    describe: 'Remove uma nota',
-    handler: function(){
-        console.log('Remove uma Nota');
+    describe: 'Remove uma nota',    
+    builder: {
+        title: {
+            describe: 'title that will be removed!',
+            demandOption: true,
+            type:'string'
+        }
+    },
+    handler(argv){
+        notes.removeNotes(argv.title)
     }
-})
+});
 
 yargs.command({
     command: 'read',
     describe: 'Ler uma Nota',
-    handler: function () {
-        console.log('Ler uma Nota');
+    builder: {
+        title: {
+            describe: 'title to be readed',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.readNotes(argv.title);
     }
-})
+});
 
 yargs.command({
     command: 'list',
     describe: 'Listar as Notas',
-    handler: function () {
-        console.log('Listar as Notas');
+    handler() {
+        notes.listNotes();
     }
-})
+});
 
 yargs.parse();
