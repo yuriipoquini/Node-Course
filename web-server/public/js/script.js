260543b66d2010d2d1266538ec29b25e3b3d807d
Gabriel Yuri Poquini Santos <gabriel.yuri.1906@gmail.com>
2019-10-08 18:10:08 -0300

    getting the location by form and printing on log

--- web-server/public/js/script.js
+++ web-server/public/js/script.js
@@ -1,8 +1,21 @@
 console.log('Alo galera, o JS ta OK!!');
 
-fetch('http://localhost:3000/weather?address=ibiuna').then((response) => {
-    response.json().then((data) => {
-        console.log(data.location);
-        console.log(data.forecast);
+const weatherForm = document.querySelector('form');
+const iptValue = document.querySelector('input').value;
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+
+    fetch(`http://localhost:3000/weather?address=${iptValue}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error)
+            } else {
+                console.log(data.location);
+                console.log(data.forecast);
+            }
+        });
     });
-});
+
+})
