a12aff1d12f33b893e45a751a053eba8ddb73c6d
Gabriel Yuri Poquini Santos <gabriel.yuri.1906@gmail.com>
2019-10-08 19:30:25 -0300

    changing language of forecast

--- web-server/src/utils/forecast.js
+++ web-server/src/utils/forecast.js
@@ -2,17 +2,17 @@ const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
 
-    const url = `https://api.darksky.net/forecast/3689980556a03b0b346815ebeb9228b6/${latitude},${longitude}?units=si&lang=pt`;
-    
-    request({ url, json: true }, (error, {body}) => {
+    const url = `https://api.darksky.net/forecast/3689980556a03b0b346815ebeb9228b6/${latitude},${longitude}?units=si`;
+
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Error to connect to weather service!');
         } else if (body.error) {
             callback('Error to find local weather!! Try another one please!');
         } else {
-        const bodyCurrently = body.currently;
+            const bodyCurrently = body.currently;
 
-        callback(undefined, body.daily.summary + ' Está aproximadamente ' + bodyCurrently.temperature + 'ºC e temos ' + bodyCurrently.precipIntensity + '% de chance de chuva!')
+            callback(undefined, body.daily.summary + ' Está aproximadamente ' + bodyCurrently.temperature + 'ºC e temos ' + bodyCurrently.precipIntensity + '% de chance de chuva!')
 
         }
     })
