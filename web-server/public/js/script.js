15ab01854ef10a88ae93003e7579f00595d6f8a9
Gabriel Yuri Poquini Santos <gabriel.yuri.1906@gmail.com>
2019-10-08 19:29:27 -0300

    Creating a auxiliar var to receive inputs value

--- web-server/public/js/script.js
+++ web-server/public/js/script.js
@@ -1,11 +1,14 @@
 console.log('Alo galera, o JS ta OK!!');
 
 const weatherForm = document.querySelector('form');
-const iptValue = document.querySelector('input').value;
+
+const weatherIpt = document.querySelector('input');
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
+
+    let iptValue = weatherIpt.value;
 
     fetch(`http://localhost:3000/weather?address=${iptValue}`).then((response) => {
         response.json().then((data) => {
