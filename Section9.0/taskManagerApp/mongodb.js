883c5396a44b3ed150bfa5036d0cf817214561f3
Gabriel Yuri Poquini Santos <gabriel.yuri.1906@gmail.com>
2019-10-17 19:37:17 -0300

    Inserting data in mongoDB

--- Section9.0/taskManagerApp/mongodb.js
+++ Section9.0/taskManagerApp/mongodb.js
@@ -2,4 +2,18 @@ const mongodb = require('mongodb');
 
 const MongodbClient = mongodb.MongoClient;
 
-const connectionURL = 'mongodb://127.0.0.1:27017'
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const databaseName = 'task-manager';
+
+MongodbClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+    if (error) {
+        console.log('Unable to connect with database');
+    }
+
+    const db = client.db(databaseName);
+
+    db.collection('users').insertOne({
+        name: 'Yuri',
+        age: 21
+    });
+});
