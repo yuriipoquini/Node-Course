5807d4dfbf8da62585706501b1fe489d989ce36c
Gabriel Yuri Poquini Santos <gabriel.yuri.1906@gmail.com>
2019-10-22 20:38:25 -0300

    Fetching with mongo

--- Section9.0/taskManagerApp/mongodb.js
+++ Section9.0/taskManagerApp/mongodb.js
@@ -1,46 +1,28 @@
-const mongodb = require('mongodb');
+// const mongodb = require('mongodb');
+// const MongodbClient = mongodb.MongoClient;
+// const ObjectID = mongodb.ObjectId;
 
-const MongodbClient = mongodb.MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
 
-MongodbClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if (error) {
-        console.log('Unable to connect with database');
+        return console.log('Unable to connect with database');
     }
 
     const db = client.db(databaseName);
 
-    // db.collection('users').insertOne({
-    //     name: 'Yuri',
-    //     age: 21
-    // });
-
-    db.collection('users').insertMany([{
-        name: 'Leo',
-        age: 20
-    }, {
-        name: 'Malu',
-        age: 21
-    }])
-
-    db.collection('tasks').insertMany([{
-            description: 'Buy chocolates',
-            completed: true
-        }, {
-            description: 'Eat chocolates',
-            completed: false
-        }, {
-            description: 'Drink water',
-            completed: false
-        }
-
-    ], (error, result) => {
+    db.collection('users').findOne({ _id: new ObjectID("5da8ebfb33925b00d8923901") }, (error, user) => {
         if (error) {
-            console.log('Unable to insert documents');
+            return console.log('Value not found. Please try another search!');
         }
 
-        console.log(result.ops);
+        console.log(user);
     })
+
+    db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
+        console.log(tasks);
+    });
 });
