60464f71eec58ce1e6688d622f505cbbddf0a939
Gabriel Yuri Poquini Santos <gabriel.yuri.1906@gmail.com>
2019-10-23 20:25:45 -0300

    Updanting data with mongo

--- Section9.0/taskManagerApp/mongodb.js
+++ Section9.0/taskManagerApp/mongodb.js
@@ -14,15 +14,31 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
 
     const db = client.db(databaseName);
 
-    db.collection('users').findOne({ _id: new ObjectID("5da8ebfb33925b00d8923901") }, (error, user) => {
-        if (error) {
-            return console.log('Value not found. Please try another search!');
-        }
+    // db.collection('users').findOne({ _id: new ObjectID("5da8ebfb33925b00d8923901") }, (error, user) => {
+    //     if (error) {
+    //         return console.log('Value not found. Please try another search!');
+    //     }
 
-        console.log(user);
+    //     console.log(user);
+    // })
+
+    // db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
+    //     console.log(tasks);
+    // });
+
+    db.collection('users').updateOne({
+        _id: new ObjectID("5da90831a8de1a3b900489b5")
+    }, {
+        $set: {
+            name: 'Miguel'
+        },
+        $inc: {
+            age: 1
+        }
+    }).then((result) => {
+        console.log(result);
+    }).catch((error) => {
+        console.log(error);
     })
 
-    db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
-        console.log(tasks);
-    });
 });
