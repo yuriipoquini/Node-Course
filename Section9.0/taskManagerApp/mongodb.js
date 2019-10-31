dcba719f54710f3c0c5efe3a3dafa3d6fc3d9967
Gabriel Yuri Poquini Santos <gabriel.yuri.1906@gmail.com>
2019-10-31 17:49:35 -0300

    finalizing CRUD

--- Section9.0/taskManagerApp/mongodb.js
+++ Section9.0/taskManagerApp/mongodb.js
@@ -1,3 +1,5 @@
+// CRUD Create Read Update Delete -> working and learning mongoDB
+
 // const mongodb = require('mongodb');
 // const MongodbClient = mongodb.MongoClient;
 // const ObjectID = mongodb.ObjectId;
@@ -14,6 +16,28 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
 
     const db = client.db(databaseName);
 
+
+
+    // -- CREATE -> INSERT --
+
+    // db.collection('users').insertMany([{
+    //         name: 'Robertinho',
+    //         age: 53
+    //     },
+    //     {
+    //         name: 'Pedrinho',
+    //         age: 47
+    //     },
+    //     {
+    //         name: 'Thiaguinho',
+    //         age: 27
+    //     }
+    // ])
+
+
+
+    // -- READ -> FIND --
+
     // db.collection('users').findOne({ _id: new ObjectID("5da8ebfb33925b00d8923901") }, (error, user) => {
     //     if (error) {
     //         return console.log('Value not found. Please try another search!');
@@ -25,6 +49,10 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
     // db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
     //     console.log(tasks);
     // });
+
+
+
+    // -- UPDATE -> UPDATE --
 
     // db.collection('users').updateOne({
     //     _id: new ObjectID("5da90831a8de1a3b900489b5")
@@ -41,16 +69,31 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
     //     console.log(error);
     // })
 
-    db.collection('tasks').updateMany({
-        completed: false
-    }, {
-        $set: {
-            completed: true
-        }
+    // db.collection('tasks').updateMany({
+    //     completed: false
+    // }, {
+    //     $set: {
+    //         completed: true
+    //     }
+    // }).then((result) => {
+    //     console.log(result);
+    // }).catch((error) => {
+    //     console.log(error);
+    // })
+
+
+    // -- DELETE -> DELETE --
+
+    // db.collection('users').deleteOne({
+    //     age: 27
+    // })
+
+
+    db.collection('tasks').deleteOne({
+        description: 'Buy chocolates'
     }).then((result) => {
         console.log(result);
     }).catch((error) => {
         console.log(error);
     })
-
 });
